868411bd7fbcb3ee816aa6d10b72f212cc28ea0c
fune0 <fune0x8@gmail.com>
2019-06-21 14:40:56 +0900

    admin画面の微調整

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -1,4 +1,5 @@
 @extends('layout.admin')
+<script src="http://code.jquery.com/jquery-3.4.1.min.js"></script>
 @section('content')
 
 <h1>これが管理画面（初回の投稿）です</h1>
--- resources/views/admin/posts/fields.blade.php
+++ resources/views/admin/posts/fields.blade.php
@@ -6,14 +6,22 @@
     <h4>情報を編集する</h4>
 
     <p>駐輪場のある区を選択してください</p>
-    <p>[memo]プルダウンで5区から選択できるようにする</p>
-    <input placeholder="区" type="text" name="ward">
+    <p>[memo]プルダウンで6区から選択できるようにする</p>
+    <select id="selectid" name="ward">
+        <option>区を選択してください</option>
+        <option value="chiyoda">千代田区</option>
+        <option value="chuo">中央区</option>
+        <option value="minato">港区</option>
+        <option value="shinjuku">新宿区</option>
+        <option value="shibuya">渋谷区</option>
+        <option value="shinagawa">品川区</option>
+    </select>
 
     <p>駐輪場名を入力してください</p>
     <input placeholder="駐輪場名" type="text" name="lotname">
 
     <p>住所を入力してください</p>
-    <input placeholder="住所" type="text" name="address">
+    <input id="disp" placeholder="住所" type="text" name="address">
 
     <p>駐輪料金を入力してください</p>
     <input placeholder="料金" type="text" name="fee">
@@ -30,3 +38,10 @@
     <input type="submit" value="登録">
     
 {{ Form::close() }}
+
+<script>
+    $("#selectid").change( function(){
+        var selectval = $("#selectid option:selected").text();
+        $("#disp").val(selectval);
+    });
+</script>
