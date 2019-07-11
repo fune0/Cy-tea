5dcd8b2d1e434908d2edff0415d25fe4024f9429
fune0 <fune0x8@gmail.com>
2019-07-11 11:25:19 +0900

    GoogleMapsAPIによる住所を座標化して表示する

--- resources/views/parking/show.blade.php
+++ resources/views/parking/show.blade.php
@@ -42,7 +42,7 @@
     <div class="show-main">
         <div class="show-txt">
             <h2>住所</h2>
-            <h3>{{ $parking->address }}</h3>
+            <h3 id="address">{{ $parking->address }}</h3>
             <h2>料金</h2>
             <h3>{{ $parking->fee }}</h3>
             <h2>収容台数</h2>
@@ -81,17 +81,35 @@
     })
     </script>
 
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBWy1hjcFApkPfZIC8osxje7Crkvw578DY"></script>
-
+    {{-- GoogleMapsAPI利用 --}}
     <script>
-    var MyLatLng = new google.maps.LatLng(35.6811673, 139.7670516);
-    var Options = {
-    zoom: 15,      //地図の縮尺値
-    center: MyLatLng,    //地図の中心座標
-    mapTypeId: 'roadmap'   //地図の種類
-    };
-    var map = new google.maps.Map(document.getElementById('map'), Options);
+    function initMap() {
+    //地図を表示する領域の div 要素のオブジェクトを変数に代入
+    var target = document.getElementById('map');  
+    //HTMLに記載されている住所の取得
+    var address = document.getElementById('address').textContent; 
+    //ジオコーディングのインスタンスの生成
+    var geocoder = new google.maps.Geocoder(); 
+    
+    //geocoder.geocode() にアドレスを渡して、コールバック関数を記述して処理
+    geocoder.geocode({ address: address }, function(results, status){
+    //ステータスが OK で results[0] が存在すれば、地図を生成
+        if (status === 'OK' && results[0]){  
+            new google.maps.Map(target, {
+            //results[0].geometry.location に緯度・経度のオブジェクトが入っている
+            center: results[0].geometry.location,
+            zoom: 15
+
+            });
+        }else{ 
+        //ステータスが OK 以外の場合や results[0] が存在しなければ、アラートを表示して処理を中断
+        alert('失敗しました。理由: ' + status);
+        return;
+        }
+    });
+    }
     </script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBWy1hjcFApkPfZIC8osxje7Crkvw578DY&callback=initMap" async defer></script>
 
 @endsection
 
